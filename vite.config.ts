57eae248811e793adb0a9adabd0375144206c2ff
HASSANE GUEDAD <115504253+hassaneGuedad@users.noreply.github.com>
2025-08-08 16:11:10 +0100

    Update and rename vite.config.js to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
@@ -32,6 +32,6 @@ export default defineConfig({
     outputFile: {
       junit: './test-results/junit.xml',
     },
-    threads: false,  // <<<< ici on désactive les threads
+    threads: false,
   },
-});
+})
